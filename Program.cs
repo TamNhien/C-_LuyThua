5bd5f5ba83fee2f8c6d5d3ffa93926b4bfc997d2
Canh Thien <canhthien91@gmail.com>
2024-05-28 19:58:51 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Nhap co so: ");
+        int co_so = int.Parse(Console.ReadLine());
+
+        Console.Write("Nhap so mu: ");
+        int mu = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"{co_so}^{mu} = {Math.Pow(co_so, mu)}");
+    }
+}
